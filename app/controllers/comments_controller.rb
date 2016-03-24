456e33a4a256a5a0088e2efd82aa5374d6935579
fedearribas <fede.arribas@gmail.com>
2016-03-24 23:47:42 +0000

    Added AJAX to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,11 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     if @comment.save
       redirect_to :back
+    #  respond_to do |format|
+    #  format.html { redirect_to :back }
+    #  format.json { head :no_content }
+    #  format.js 
+    #  end
     end
   end
   
@@ -13,7 +18,14 @@ class CommentsController < ApplicationController
     pst = Post.find(params[:post_id])
     @comment = pst.comments.find(params[:id])
     @comment.destroy
-    redirect_to :back
+    ##redirect_to :back
+    
+    respond_to do |format|
+    format.html { redirect_to :back }
+    format.json { head :no_content }
+    format.js   { render :layout => false }
+    end
+    
   end
   
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,21 @@ class PostsController < ApplicationController
   
   before_action :get_user
   before_action :authenticate_user!
+  
+
 
   def create
     @post = current_user.posts.new(posts_param)
     @post.profile_posted = @user.id
     if @post.save
-      flash[:success] = "Post successfully added!"
-      redirect_to :back
+      #flash[:success] = "Post successfully added!"
+      #redirect_to :back
+           
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { head :no_content }
+      format.js
+    end
     else
       flash[:danger] = "There was a problem adding the post"
     end
@@ -17,8 +25,14 @@ class PostsController < ApplicationController
   def destroy
     @post = @user.posts.find(params[:id])
     @post.destroy
-    flash[:success] = "Post deleted."
-    redirect_to :back
+    #flash[:success] = "Post deleted."
+    #redirect_to :back
+        
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
   end
   
   
