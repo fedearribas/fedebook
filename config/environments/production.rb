dcd9e6a24debe59024a26997e39b9856ff12c7d8
fedearribas <fede.arribas@gmail.com>
2016-03-28 03:02:19 +0000

    Added assets precompile option in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-RAILS_ENV=production bundle exec rake assets:precompile
+  RAILS_ENV=production bundle exec rake assets:precompile
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -30,6 +30,11 @@ RAILS_ENV=production bundle exec rake assets:precompile
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+
+  
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
