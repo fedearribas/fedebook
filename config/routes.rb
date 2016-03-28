d061edf866bbaa81e6e59fb0d3554b441f2b7a60
fedearribas <fede.arribas@gmail.com>
2016-03-28 02:04:55 +0000

    Added log in and sign up buttons to home page

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     resources :friendships, only: [:show, :create, :destroy]
   
   # You can have the root of your site routed with "root"
-   root 'dashboard#index'
+   root to: 'dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
