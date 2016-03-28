e6150a4e1b09c64e78f927992a38b878a599a697
fedearribas <fede.arribas@gmail.com>
2016-03-28 02:57:32 +0000

    Added assets precompile option in production

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,14 @@ class Post < ActiveRecord::Base
     belongs_to :profileposted, class_name: "User"
     has_many :comments, dependent: :destroy
     
+    
      has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "avatar/missing.jpg"
      validates_attachment :avatar,
     #:presence => true,
     :size => { :in => 0..10.megabytes },
+    :storage => :dropbox,
     :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/ }
+    
     
     default_scope { order(created_at: :desc) }
     
